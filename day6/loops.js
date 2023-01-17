97f91efe2c774565651803898a6c8abd5ae51c04
charlespaul1 <charlespaulwabomba@gmail.com>
2023-01-17 17:41:43 +0300

    loops

---
+++ day6/loops.js
@@ -0,0 +1,31 @@
+//iterate 0  to 20 using for loop
+for (let i = 0; i < 21; i++){
+    console.log(i)
+}
+//iterate 20 to 0
+for (let i = 20; i > 0; i--){
+    console.log(i)
+}
+// a loop that makes a triangle pattern
+for (let i = 1; i <= 7; i++){
+    for(let j = 0; j < i; j++){
+        console.log('#')
+    }
+    console.log('\n')
+}
+//printing a multiplication table
+let table = 10;
+let result;
+for (let i = 0; i < 11; i++){
+    //for (let j = 0; j < i; j++)
+    document.write(i + ' x ' + table + ' = ' + i * table + '<br>')
+}
+//iterating from 0 to 100 & print even numbers
+for (let i = 0; i <= 100; i++){
+    if (i % 2 == 0)
+    document.write(i + ' ')
+}
+//iteratiing 0 to 100 & printing prime numbers
+for (let i = 0; i <= 100; i++){
+    if 
+}
