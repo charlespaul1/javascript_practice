fb4ee7f484d9ea26259313e245b245643078e135
charlespaul1 <charlespaulwabomba@gmail.com>
2023-02-07 08:37:58 +0300

    object methods and manipulation

--- day9/main.js
+++ day9/main.js
@@ -43,6 +43,40 @@ person.title = 'cloud engineer'
 person.skills.push('kubernetes')
 person.skills.push('sass')
 console.log(person)
-//getting skills without last skills
-let skillWithoutLastSkill = this.skills.splice(0,this.skills.length - 1).join(', ')
-console.log(skillWithoutLastSkill)
+//manipulating an object
+//Object.assign: To copy an object without modifying the original object
+const car = {
+    model: 'jeep wrangler',
+    year: 2022,
+    color: 'grey',
+    capacity: '4 seater',
+    engine: '250 cc'
+}
+//copying car properties to  using Object.assign
+const newCar = Object.assign({}, car)
+console.log(newCar)
+//getting keys or properties of an object as an array
+const keys  = Object.keys(newCar)
+console.log(keys)
+//getting values of an object as an array using Object.values
+const values = Object.values(car)
+console.log(values)
+//getting keys and values of an objet using Object.entries
+const myEntries = Object.entries(car)
+console.log(myEntries)
+//checking if a specific key or property exist in an object
+console.log(newCar.hasOwnProperty('model'))
+const dog = {};
+console.log(dog)
+//adding properties to dog object
+dog.name = 'rex'
+dog.legs = 4
+dog.color = 'black'
+dog.age =  '6 months'
+dog.bark = function(){
+    return 'woof woof'
+};
+let dogValues = Object.values(dog)
+console.log(dogValues)
+dog.breed = 'german shepherd'
+
