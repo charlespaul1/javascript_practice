d2f5e73f618d2bbac88d0891045316552ad240eb
charlespaul1 <charlespaulwabomba@gmail.com>
2023-03-07 21:48:30 +0300

    tackled destructing arrays using iteration, spread

---
+++ day11/README.md
@@ -0,0 +1,1 @@
+learning destructuring and spread
---
+++ day11/spreadOperator.js
@@ -0,0 +1,27 @@
+// destructing an array
+const myArray = [1, 2, 4, 5]
+let [num1, num2, num3, num4] = myArray
+console.log(num3)
+const fullStack = [['html', 'css', 'js', 'react'], ['Node', 'Express', 'MongoDB']]
+let [frontEnd, backEnd] = fullStack
+console.log(backEnd)
+
+
+// if you want to skip an item in the array use additional commas
+const myStack = ['html', 'css', 'react', 'figma', 'javascript']
+let [skill1, skill2, , skill4] = myStack
+
+console.log(skill1, skill2, skill4)
+
+
+// using spread operator
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9]
+
+let [namba1, namba2, ...namba4] = numbers
+console.log(namba4)
+
+// destructing using iteration
+const countries = [['Nigeria', 'Lagos'], ['Ghana', 'Accra'], ['Kenya', 'Nairobi'], ['South Africa', 'Cape town'], ['Tanzania', 'Dodoma'], ['Uganda', 'Kampala']]
+for (let[Country, City] of countries){
+    console.log(Country, City)
+}
